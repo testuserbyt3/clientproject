bbd46987d9c062846c8373026fd1c5be94c87417
mohammadhamzailtaf <144703210+mohammadhamzailtaf@users.noreply.github.com>
2023-09-15 12:32:49 -0700

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,345 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap">
+          <div className="component">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="overlap-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="overlap-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="div-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="div-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="component-2">
+            <div className="overlap-2">
+              <div className="funday-home-banner" />
+              <img
+                className="banner-home"
+                alt="Banner home"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/banner-home-2-removebg-preview.png"
+              />
+              <div className="text-wrapper-3">Redmi 12C</div>
+              <div className="text-wrapper-4">Early Deal Special</div>
+              <p className="from">
+                <span className="span">From </span>
+                <span className="text-wrapper-5">₹2,399*</span>
+              </p>
+              <div className="rectangle" />
+              <div className="text-wrapper-6">Buy Now</div>
+              <div className="rectangle-2" />
+              <div className="text-wrapper-7">Just Launched</div>
+              <img
+                className="logo-removebg"
+                alt="Logo removebg"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/logo-removebg-preview.png"
+              />
+            </div>
+          </div>
+          <div className="component-3">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="component-4">
+            <div className="text-wrapper-8">Xiaomi 13 Pro</div>
+            <img
+              className="xiomi-pro"
+              alt="Xiomi pro"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/xiomi-13-pro-removebg-preview.png"
+            />
+          </div>
+          <div className="component-5">
+            <div className="overlap-group">
+              <div className="text-wrapper">₹4,999</div>
+              <div className="text-wrapper-2">₹79,999</div>
+            </div>
+          </div>
+          <div className="component-6">
+            <img
+              className="img"
+              alt="Funday home banner"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/funday-home-banner-removebg-preview.png"
+            />
+            <div className="overlap-3">
+              <p className="text-wrapper-9">Redmi Note 12 Pro 5G</p>
+              <img
+                className="redmi-note-pro"
+                alt="Redmi note pro"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/redmi-note-12-pro-5g-removebg-preview.png"
+              />
+              <div className="element-off-wrapper">
+                <div className="text-wrapper-10">93% off</div>
+              </div>
+              <div className="overlap-group-2">
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+                <div className="redmi-note-pro-g">₹2,499</div>
+              </div>
+              <div className="redmi-note-pro-2">₹16,999</div>
+            </div>
+            <img
+              className="poster"
+              alt="Poster"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6504ae538dc332f319c3d0f0/img/poster3@2x.png"
+            />
+            <div className="overlap-4">
+              <div className="text-wrapper-11">Our Services</div>
+              <div className="text-wrapper-12">Trusted. Secure . Reliable</div>
+            </div>
+            <div className="text-wrapper-13">XIAOMI CARE</div>
+            <div className="text-wrapper-14">Extended Warranty</div>
+            <div className="text-wrapper-15">Screen Protect</div>
+            <div className="text-wrapper-16">Complete Protection</div>
+            <div className="overlap-5">
+              <div className="text-wrapper-17">Follow Mi</div>
+              <div className="let-s-stay-in-touch">Let&#39;s stay in touch</div>
+              <div className="overlap-6">
+                <img
+                  className="element-google"
+                  alt="Element google"
+                  src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/867-8678850-google-play-store-transparent-google-play-icon-remov@2x.png"
+                />
+                <div className="text-wrapper-18">Download on Google play</div>
+              </div>
+              <div className="overlap-7">
+                <div className="text-wrapper-19">Enter email address</div>
+                <img
+                  className="icon-material"
+                  alt="Icon material"
+                  src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/icon-material-navigate-next.svg"
+                />
+              </div>
+              <div className="text-wrapper-20">Get mi store app</div>
+              <div className="text-wrapper-21">Sitemap</div>
+              <img
+                className="line"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-1.svg"
+              />
+              <p className="p">Copyright © 2010 - 2023 Xiaomi. All Rights Reserved</p>
+            </div>
+            <div className="component-7">
+              <img
+                className="logo-removebg-2"
+                alt="Logo removebg"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/logo-removebg-preview.png"
+              />
+              <img
+                className="search-button"
+                alt="Search button"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/search-button-removebg-preview.png"
+              />
+              <img
+                className="cart-butoon-removebg"
+                alt="Cart butoon removebg"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/cart-butoon-removebg-preview.png"
+              />
+              <img
+                className="menu-button"
+                alt="Menu button"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/menu-button@2x.png"
+              />
+            </div>
+            <div className="overlap-8">
+              <div className="component-8" />
+              <img
+                className="categroires-icon"
+                alt="Categroires icon"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/categroires-icon-removebg-preview.png"
+              />
+            </div>
+            <div className="component-9">
+              <div className="text-wrapper-10">95% off</div>
+            </div>
+            <div className="redmi-note-pro-3">₹4,999</div>
+            <div className="redmi-note-pro-4">₹20,000</div>
+            <div className="overlap-9">
+              <div className="element-off">93% off</div>
+              <div className="rectangle-3" />
+              <div className="xiaomi-pro-wrapper">
+                <div className="text-wrapper-8">Redmi Note 12</div>
+              </div>
+              <img
+                className="pms"
+                alt="Pms"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/pms-1680028893-42691134-380x380-removebg-preview.png"
+              />
+              <div className="rectangle-4" />
+              <div className="text-wrapper-22">95% off</div>
+              <div className="redmi-note-pro-5">₹2,499</div>
+              <div className="redmi-note-pro-6">₹30,000</div>
+              <div className="redmi-note-pro-6">₹30,000</div>
+            </div>
+            <div className="overlap-10">
+              <div className="element-off">93% off</div>
+              <div className="rectangle-3" />
+              <div className="xiaomi-pro-wrapper">
+                <div className="text-wrapper-8">Redmi 11 Prime 5G</div>
+              </div>
+              <div className="rectangle-4" />
+              <div className="text-wrapper-22">96% off</div>
+              <div className="redmi-note-pro-5">₹2,999</div>
+              <img
+                className="pms-2"
+                alt="Pms"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6504ae538dc332f319c3d0f0/img/pms-1662446272-08342530-380x380-removebg-preview.png"
+              />
+            </div>
+            <div className="overlap-11">
+              <div className="text-wrapper-9">Redmi Note 11</div>
+              <div className="overlap-12">
+                <div className="element-off-2">93% off</div>
+                <div className="rectangle-5" />
+                <div className="rectangle-5" />
+                <div className="element-off-2">96% off</div>
+              </div>
+              <div className="overlap-13">
+                <div className="redmi-note-pro-g">₹2,199</div>
+                <div className="redmi-note-pro-7">₹80,000</div>
+              </div>
+              <img
+                className="pms-3"
+                alt="Pms"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/pms-1644212124-59044616-380x380-removebg-preview.png"
+              />
+            </div>
+            <div className="overlap-14">
+              <div className="text-wrapper-9">Redmi K50i 5G</div>
+              <div className="overlap-12">
+                <div className="element-off-2">93% off</div>
+                <div className="rectangle-5" />
+                <div className="rectangle-5" />
+                <div className="element-off-2">97% off</div>
+              </div>
+              <div className="overlap-15">
+                <div className="redmi-note-pro-g">₹3,499</div>
+                <div className="redmi-note-pro-8">₹87,000</div>
+              </div>
+              <img
+                className="removebg"
+                alt="Removebg"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/k50i-removebg-preview.png"
+              />
+            </div>
+            <div className="overlap-16">
+              <div className="text-wrapper-9">Redmi 12C</div>
+              <div className="overlap-17">
+                <div className="element-off-3">93% off</div>
+                <div className="rectangle-6" />
+                <div className="rectangle-6" />
+                <div className="element-off-2">92% off</div>
+              </div>
+              <div className="redmi-note-pro-9">₹2,399</div>
+              <div className="redmi-note-pro-2">₹80,000</div>
+              <img
+                className="pms-4"
+                alt="Pms"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/pms-1680030998-0464825-380x380-removebg-preview.png"
+              />
+            </div>
+            <div className="overlap-18">
+              <div className="text-wrapper-9">Redmi 11i 5G</div>
+              <div className="overlap-17">
+                <div className="element-off-3">93% off</div>
+                <div className="rectangle-6" />
+                <div className="rectangle-6" />
+                <div className="element-off-2">98% off</div>
+              </div>
+              <div className="redmi-note-pro-9">₹2,799</div>
+              <div className="redmi-note-pro-2">₹80,000</div>
+              <img
+                className="pms-5"
+                alt="Pms"
+                src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65030903875ec22a96bf51c9/img/pms-1641441296-82974884-380x380-removebg-preview.png"
+              />
+            </div>
+          </div>
+          <div className="component-10">
+            <img
+              className="removebg-preview"
+              alt="Removebg preview"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/s1-removebg-preview.png"
+            />
+            <img
+              className="s-removebg-preview"
+              alt="Removebg preview"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/s2-removebg-preview.png"
+            />
+            <img
+              className="removebg-preview-2"
+              alt="Removebg preview"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6504ae538dc332f319c3d0f0/img/s3-removebg-preview@2x.png"
+            />
+          </div>
+          <div className="component-11">
+            <img
+              className="fb-icon-removebg"
+              alt="Fb icon removebg"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/fb-icon-removebg-preview.png"
+            />
+            <img
+              className="insta-icon-removebg"
+              alt="Insta icon removebg"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/insta-icon-removebg-preview.png"
+            />
+            <img
+              className="twitter-icon"
+              alt="Twitter icon"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/twitter-icon-removebg-preview.png"
+            />
+          </div>
+          <div className="component-12">
+            <img
+              className="download-removebg"
+              alt="Download removebg"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/download-removebg-preview.png"
+            />
+            <div className="text-wrapper-23">Xiaomi India</div>
+          </div>
+        </div>
+        <div className="component-13">
+          <p className="text-wrapper-24">Sale Is Live | Upto 98% Discount On Phones</p>
+        </div>
+      </div>
+    </div>
+  );
+};
